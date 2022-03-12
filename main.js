efe01194043d8b93cf89320aaef08f7daa2c9157
Vinicius Moreira <79025625+vinicius6433@users.noreply.github.com>
2022-03-12 16:33:53 -0300

    Atualização Instragram

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const linksSocialMedia = {
   youtube: 'vinicius6433',
   facebook: 'vinicius.moreira.1650',
   twitter: 'vinicius64333',
-  instagram: 'vinicius_moreira6433'
+  instagram: 'vinicius64333'
 }
 function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
